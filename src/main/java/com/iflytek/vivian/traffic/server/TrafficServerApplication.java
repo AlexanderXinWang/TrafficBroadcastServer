6e02ea9088bae5a1bacc5b4f40489f54eb7e7134
AlexanderWang <Alexander.Xin.Wang@outlook.com>
2021-01-04 10:39:23 +0800

    Initial commit

---
+++ src/main/java/com/iflytek/vivian/traffic/server/TrafficServerApplication.java
@@ -0,0 +1,21 @@
+package com.iflytek.vivian.traffic.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EntityScan("com.iflytek.vivian.traffic")
+@ComponentScan("com.iflytek.vivian.traffic")
+@EnableJpaRepositories("com.iflytek.vivian.traffic.server.domain.dao")
+@EnableSwagger2
+public class TrafficServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficServerApplication.class, args);
+    }
+
+}
