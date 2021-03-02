c19ca2f919d00addff4021e03c0b9a031b315f3a
AlexanderWang <Alexander.Xin.Wang@outlook.com>
2021-03-02 09:36:08 +0800

    添加face数据传输对象

---
+++ src/main/java/com/iflytek/vivian/traffic/server/dto/face/FdbImageExtraProp.java
@@ -0,0 +1,16 @@
+package com.iflytek.vivian.traffic.server.dto.face;
+
+import lombok.Data;
+
+/**
+ * @ClassName FdbImageExtraProp
+ * @Description TODO
+ * @Author xinwang41
+ * @Date 2021/3/2 9:34
+ * @Description：传递给人脸库的拓展信息
+ **/
+
+@Data
+public class FdbImageExtraProp {
+    private String userId;
+}
