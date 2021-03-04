0edabdf646e4e0fc8c9d6a239c938e27d996c352
AlexanderWang <Alexander.Xin.Wang@outlook.com>
2021-03-04 10:54:18 +0800

    补充nlp服务功能

--- src/main/java/com/iflytek/vivian/traffic/server/domain/service/NlpService.java
+++ src/main/java/com/iflytek/vivian/traffic/server/domain/service/NlpService.java
@@ -1,6 +1,7 @@
 package com.iflytek.vivian.traffic.server.domain.service;
 
 import com.iflytek.vivian.traffic.server.domain.entity.Event;
+import com.iflytek.vivian.traffic.server.dto.EventDto;
 import com.iflytek.vivian.traffic.server.dto.Position;
 import com.iflytek.vivian.traffic.server.utils.StringUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +24,58 @@ public class NlpService {
     @Autowired
     private StringUtil stringUtil;
 
+    /**
+     * 警情概要生成
+     * @param text
+     * @return
+     */
+    public EventDto getEventCase(String text) {
+        long startTime = System.currentTimeMillis();
+        // 去空格
+        text = StringUtil.textPreprocess(text);
+
+        EventDto eventDto = new EventDto();
+
+        try {
+            Map<String, Position> map = stringUtil.getPoliceCaseElement(text);
+
+            // 发生地
+            Position positionLocation = map.get("location");
+            if (null != positionLocation){
+                eventDto.setLocation(text.substring(positionLocation.getStartPos(), positionLocation.getEndPos()));
+            }
+
+            // 车辆类型
+            Position positionVehicle = map.get("vehicle");
+            if (null != positionVehicle){
+                eventDto.setVehicle(text.substring(positionVehicle.getStartPos(), positionVehicle.getEndPos()));
+            }
+
+            // 事件
+            Position positionEvent = map.get("event");
+            if (null != positionEvent){
+                eventDto.setEvent(text.substring(positionEvent.getStartPos(), positionEvent.getEndPos()));
+            }
+
+            // 事件结果
+            Position positionEventResult = map.get("eventResult");
+            if (null != positionEventResult){
+                eventDto.setEventResult(text.substring(positionEventResult.getStartPos(), positionEventResult.getEndPos()));
+            }
+
+        } catch (Exception e) {
+            log.error("nlp解析服务调用失败 ！", e.getMessage());
+        }
+        long endTime = System.currentTimeMillis();
+        log.info(">>>>>>nlp解析服务，警情概要生成, 耗时{}ms.", endTime - startTime);
+        return eventDto;
+    }
+
+    /**
+     * 得到单句的要素
+     * @param text
+     * @return
+     */
     public Event getEventCaseSimple(String text) {
         long startTime = System.currentTimeMillis();
 
